ee94754863c44f840dc4c9194fb29f70e04223b0
GOVARTHAN R T <114054480+govarthan9669@users.noreply.github.com>
2023-07-11 09:24:47 +0530

    Add files via upload

---
+++ Day 3/src/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+
+{/* <Router></Router>
+
+<Router basename="/react-auth-ui/">
+<Route exact path="/s" component={SignUpForm} />
+<Route path="/si" component={SignInForm} />
+
+<Route exact path="/v" component={VendorSignUP} />
+<Route path="/vi" component={VendorSignIn} />
+
+<Route exact path="/" component={MainLoginPageVendor} />
+<Route path="/ven" component={MainLoginPageUser} />
+
+<Route path="/VendorSign" component={VendorSignUP} />
+<Route path="/UserSign" component={SignUpForm} />
+
+
+
+</Router> */}
