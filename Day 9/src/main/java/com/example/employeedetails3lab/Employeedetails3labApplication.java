869fc97a29f42217d86587a2ad384b085d7f2bcb
GOVARTHAN R T <114054480+govarthan9669@users.noreply.github.com>
2023-07-20 10:49:29 +0530

    Add files via upload

---
+++ Day 9/src/main/java/com/example/employeedetails3lab/Employeedetails3labApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeedetails3lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Employeedetails3labApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Employeedetails3labApplication.class, args);
+	}
+
+}
